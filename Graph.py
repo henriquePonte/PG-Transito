896092a451deff64ec7b948a9a2c79631448d207
Henrique Ponte <107682582+henriquePonte@users.noreply.github.com>
2024-01-15 15:27:50 -0100

    feito no geral

--- Graph.py
+++ Graph.py
@@ -28,79 +28,15 @@ def create_graph_from_json(json_file):
     return G
 
 
-def generate_traffic_intersections(G):
-    # Convert set of nodes to list
-    nodes_list = list(G.nodes())
-
-    # Randomly select 6 intersections
-    intersections = random.sample(nodes_list, 4)
-
-    # Assign exactly 2 high traffic intersections and 4 medium traffic intersections
-    high_traffic_intersections = random.sample(intersections, 2)
-    medium_traffic_intersections = list(set(intersections) - set(high_traffic_intersections))
-
-    # Create a list of all edges that have high traffic intersections
-    high_traffic_edges = [edge for edge in G.edges() if
-                          edge[0] in high_traffic_intersections or edge[1] in high_traffic_intersections]
-
-    # Create a list of all edges that have medium traffic intersections
-    medium_traffic_edges = [edge for edge in G.edges() if
-                            edge[0] in medium_traffic_intersections or edge[1] in medium_traffic_intersections]
-
-    # Update edge weights for high traffic intersections
-    for edge in high_traffic_edges:
-        G[edge[0]][edge[1]]['cost'] += 1
-
-    # Update edge weights for medium traffic intersections
-    for edge in medium_traffic_edges:
-        G[edge[0]][edge[1]]['cost'] += 0.3
-
-    return G
-
-
-def draw_graph(G):
-    pos = nx.get_node_attributes(G, 'pos')
-
-    # Set different colors for each node
-    colors = plt.cm.rainbow(np.linspace(0, 1, len(G.nodes())))
-
-    # Adjust the size of the nodes
-    node_size = 60
-
-    # Draw edges with different colors based on traffic
-    edge_colors = []
-    for edge in G.edges():
-        if G[edge[0]][edge[1]]['cost'] == 1:
-            edge_colors.append('red')  # High traffic
-        elif G[edge[0]][edge[1]]['cost'] == 0.3:
-            edge_colors.append('orange')  # Medium traffic
-        else:
-            edge_colors.append('green')  # Default color for other edges
-
-    nx.draw(G, pos, with_labels=False, node_color=colors, node_size=node_size, edge_color=edge_colors)
-
-    # Create a legend
-    legend_elements = [Line2D([0], [0], marker='o', color='w', markerfacecolor=colors[i], markersize=8,
-                              label=node) for i, node in enumerate(G.nodes())]
-
-    # Add custom legend for edge colors
-    legend_elements.append(Line2D([0], [0], color='w', markerfacecolor='red', markersize=8, label='High Traffic'))
-    legend_elements.append(Line2D([0], [0], color='w', markerfacecolor='orange', markersize=8, label='Medium Traffic'))
-
-    plt.legend(handles=legend_elements, loc='upper left', title='Points', fontsize=8)
-    plt.show()
-
-
 def create_and_draw_graph(json_file):
     G = create_graph_from_json(json_file)
-    G = generate_traffic_intersections(G)
 
     fig, ax = plt.subplots()
     draw_graph(G)
     return fig
 
 
-def draw_graph_teste(G, path_edges=None):
+def draw_graph(G, path_edges=None):
     pos = nx.get_node_attributes(G, 'pos')
 
     # Set different colors for each node
@@ -112,12 +48,14 @@ def draw_graph_teste(G, path_edges=None):
     # Draw edges with different colors based on traffic
     edge_colors = []
     for edge in G.edges():
-        if G[edge[0]][edge[1]]['cost'] == 1:
-            edge_colors.append('red')  # High traffic
-        elif G[edge[0]][edge[1]]['cost'] == 0.3:
+        if G[edge[0]][edge[1]]['cost'] < 5:
+            edge_colors.append('green')  # Light traffic
+        elif G[edge[0]][edge[1]]['cost'] < 10:
             edge_colors.append('orange')  # Medium traffic
+        elif 10 <= G[edge[0]][edge[1]]['cost'] <= 30:
+            edge_colors.append('yellow')  # Moderate traffic
         else:
-            edge_colors.append('green')  # Default color for other edges
+            edge_colors.append('red')  # High traffic
 
     nx.draw(G, pos, with_labels=False, node_color=colors, node_size=node_size, edge_color=edge_colors)
 
--- Simulacao.py
+++ Simulacao.py
@@ -1,5 +1,6 @@
 import json
 import tkinter as tk
+import time
 from matplotlib import pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import H_star
@@ -24,6 +25,22 @@ class GraphWindow:
         self.canvas.draw()
         self.canvas.get_tk_widget().pack()
 
+        # Adiciona um rótulo para exibir a hora
+        self.time_label = tk.Label(self.root, text="")
+        self.time_label.pack()
+
+        # Inicializa a hora simulada com a hora atual
+        self.simulated_time = time.localtime()
+        self.update_time()
+
+        # Adiciona um widget de entrada para alterar a hora
+        self.time_entry = tk.Entry(self.root)
+        self.time_entry.pack()
+
+        # Adiciona um botão para definir a hora
+        set_time_button = tk.Button(self.root, text="Definir hora", command=self.set_time)
+        set_time_button.pack()
+
         start_label = tk.Label(self.root, text="Digite a cidade de início ou 'sair' para parar: ")
         start_label.pack()
         self.start_entry = tk.Entry(self.root)
@@ -37,9 +54,76 @@ class GraphWindow:
         start_button = tk.Button(self.root, text="Iniciar busca",
                                  command=self.start_search_and_draw_path)
         start_button.pack()
+        self.G = Graph.create_graph_from_json("PontosTuristicos.conf")  # Armazene o grafo como um atributo da classe
+
+    def update_time(self):
+        # Atualiza o rótulo da hora com a hora simulada
+        self.time_label.config(text=time.strftime("%H:%M:%S", self.simulated_time))
+        self.root.after(1000, self.update_time)  # Atualiza a hora a cada 1 segundo
+
+    def update_costs(self):
+        # Obtém a hora atual
+        current_hour = self.simulated_time.tm_hour
+
+        # Ajusta os custos das conexões com base na hora do dia
+        if 8 <= current_hour <= 11 or 16 <= current_hour <= 19:
+            for conexao in self.cities["conexoes"]:
+                conexao["cost"] += 2
+                # Atualiza o custo da conexão correspondente no grafo
+                self.G[conexao["origem"]][conexao["destino"]]['cost'] += 2
+                self.G[conexao["destino"]][conexao["origem"]]['cost'] += 2
+        else:
+            for conexao in self.cities["conexoes"]:
+                conexao["cost"] = max(0, conexao["cost"] - 2)
+                # Atualiza o custo da conexão correspondente no grafo
+                self.G[conexao["origem"]][conexao["destino"]]['cost'] = max(0, self.G[conexao["origem"]][
+                    conexao["destino"]]['cost'] - 2)
+                self.G[conexao["destino"]][conexao["origem"]]['cost'] = max(0, self.G[conexao["destino"]][
+                    conexao["origem"]]['cost'] - 2)
+
+    def set_time(self):
+        # Define a hora simulada de acordo com a entrada do usuário
+        time_string = self.time_entry.get()
+        try:
+            new_time = time.strptime(time_string, "%H:%M:%S")  # Verifica se a hora está no formato correto
+            # Atualiza a hora simulada, mantendo a data atual
+            self.simulated_time = time.struct_time(
+                (self.simulated_time.tm_year, self.simulated_time.tm_mon, self.simulated_time.tm_mday,
+                 new_time.tm_hour, new_time.tm_min, new_time.tm_sec,
+                 self.simulated_time.tm_wday, self.simulated_time.tm_yday, self.simulated_time.tm_isdst))
+            # Atualiza os custos das conexões
+            self.update_costs()
+            # Atualiza o gráfico
+            self.fig = plt.figure()  # Cria uma nova figura
+            Graph.draw_graph(self.G)  # Desenha o gráfico na figura
+
+            # Remove o canvas antigo
+            self.canvas.get_tk_widget().pack_forget()
+
+            # Cria um novo canvas e adiciona à janela Tkinter
+            self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)
+            self.canvas.draw()
+            self.canvas.get_tk_widget().pack()
+
+        except ValueError:
+            print("Hora inválida! Por favor, insira a hora no formato HH:MM:SS.")
 
     def start_search_and_draw_path(self):
-        path_edges = H_star.start_search(self.start_entry.get(), self.goal_entry.get(), self.cities)
+        start_city = self.start_entry.get()
+        goal_city = self.goal_entry.get()
+
+        # Obtém a hora atual
+        current_hour = self.simulated_time.tm_hour
+
+        # Cria uma cópia das cidades para não modificar os dados originais
+        cities_copy = self.cities.copy()
+
+        # Ajusta os custos das conexões com base na hora do dia
+        if 12 <= current_hour <= 18:
+            for conexao in cities_copy["conexoes"]:
+                conexao["cost"] *= 2
+
+        path_edges = H_star.start_search(start_city, goal_city, cities_copy)
         self.draw_path(path_edges)
 
     def draw_path(self, path_edges):
@@ -47,7 +131,31 @@ class GraphWindow:
         self.fig = plt.figure()
 
         G = Graph.create_graph_from_json("./PontosTuristicos.conf")
-        Graph.draw_graph_teste(G, path_edges)
+        Graph.draw_graph(G, path_edges)
+
+        # Remove o canvas antigo
+        self.canvas.get_tk_widget().pack_forget()
+
+        # Cria um novo canvas e adiciona à janela Tkinter
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)
+        self.canvas.draw()
+        self.canvas.get_tk_widget().pack()
+
+    def advance_time(self):
+        # Avança a hora simulada em 30 minutos
+        new_time = time.localtime(time.mktime(self.simulated_time) + 30 * 60)
+        self.simulated_time = time.struct_time(
+            (new_time.tm_year, new_time.tm_mon, new_time.tm_mday,
+             new_time.tm_hour, new_time.tm_min, new_time.tm_sec,
+             new_time.tm_wday, new_time.tm_yday, new_time.tm_isdst))
+        # Atualiza os custos das conexões
+        self.update_costs()
+        # Atualiza o gráfico
+        self.update_graph()
+
+    def update_graph(self):
+        self.fig = plt.figure()  # Cria uma nova figura
+        Graph.draw_graph(self.G)  # Desenha o gráfico na figura
 
         # Remove o canvas antigo
         self.canvas.get_tk_widget().pack_forget()
@@ -58,6 +166,8 @@ class GraphWindow:
         self.canvas.get_tk_widget().pack()
 
     def run(self):
+        self.advance_time()
+        self.root.after(5000, self.run) 
         self.root.mainloop()
 
 
